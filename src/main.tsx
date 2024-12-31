fa091dd795c7c9ae550ee4f7add0c716052606d7
Usama Ansari <usamazansari@outlook.com>
2024-12-31 12:59:29 +0530

    chore: rename app to App

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createTheme, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { App } from './app/app';
+import { App } from './app/App';
 import './styles.css';
 
 const root = ReactDOM.createRoot(
