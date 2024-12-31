8d3f3134d21e5928e5c540bb5402592eff92f952
Usama Ansari <usamazansari@outlook.com>
2024-12-31 12:55:46 +0530

    chore: use mantine

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,32 @@
-import NxWelcome from './nx-welcome';
-import '../styles.css';
+import { Box, Button, Stack, Text } from '@mantine/core';
+import { useInterval, useShallowEffect } from '@mantine/hooks';
+import * as React from 'react';
 
 export function App() {
+  const [seconds, setSeconds] = React.useState(0);
+  const { start, stop, active, toggle } = useInterval(
+    () =>
+      setSeconds((s) => {
+        return s + 1;
+      }),
+    1000
+  );
+
+  useShallowEffect(() => {
+    start();
+    return stop;
+  }, []);
+
   return (
-    <div>
-      <NxWelcome title="freedom-of-travel" />
-    </div>
+    <Box className="grid h-screen place-content-center">
+      <Stack align="center">
+        <Text>
+          Page loaded <b>{seconds}</b> seconds ago
+        </Text>
+        <Button onClick={toggle} color={active ? 'red' : 'teal'}>
+          {active ? 'Stop' : 'Start'} counting
+        </Button>
+      </Stack>
+    </Box>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import { App } from './app/app';
+import './styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 root.render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>
 );
